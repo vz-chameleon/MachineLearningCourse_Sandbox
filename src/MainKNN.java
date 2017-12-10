0dada557126b66abe1d73fc680ef8d29362b4457
GTimothy <timothy.garwood@protonmail.com>
2017-12-10 18:00:48 +0100

    using nested cross-validation and a parameter space in main

--- src/MainKNN.java
+++ src/MainKNN.java
@@ -1,16 +1,42 @@
+import java.util.ArrayList;
+
 import data.Dataset;
 import distances.ManhattanDistance;
 import knn.KNearestNeighbours;
+import nested_cross_validation.NestedCV;
+import nested_cross_validation.ParameterSpace;
 
 public class MainKNN {
 
 	public static void main(String[] args) {
 		
 		Dataset iris=new Dataset();
+		System.out.println(iris);
 		iris.loadFromCSVFile("ressources/data/iris.data");
-		KNearestNeighbours knnClassifier = new KNearestNeighbours(5, new ManhattanDistance());
+//		iris.normalize();
+
+
+		KNearestNeighbours knnClassifier = new KNearestNeighbours(5, new ManhattanDistance()); //the parameter  '5' will be overridden in the nested cross-validation
+				
+		NestedCV nestedCrossValidation = new NestedCV(5);
 		
-		//TODO : Test with training set
+		ParameterSpace parameterSpace = new ParameterSpace();
+		ArrayList<Double> k_num_neighbours_possibilities = new ArrayList<>();
+//		k_num_neighbours_possibilities.add((double) 20);
+//		k_num_neighbours_possibilities.add((double) 1);
+//		k_num_neighbours_possibilities.add((double) 2);
+		k_num_neighbours_possibilities.add((double) 3);
+		k_num_neighbours_possibilities.add((double) 4);
+		k_num_neighbours_possibilities.add((double) 5);
+		k_num_neighbours_possibilities.add((double) 6);
+		k_num_neighbours_possibilities.add((double) 7);
+		k_num_neighbours_possibilities.add((double) 8);
+		parameterSpace.addParameterAndValues("k_num_neighbours", k_num_neighbours_possibilities);
+		
+		nestedCrossValidation.NestedCrossValidate(knnClassifier, parameterSpace, iris, false);
+		
+		System.out.println("\n\nNested cross-validation results :");
+		System.out.println(nestedCrossValidation);
 		
 
 	}
